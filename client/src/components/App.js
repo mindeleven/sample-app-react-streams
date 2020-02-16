04fee0383daf78db688346e92f6907f215756cc2
Jürgen Kober <mindeleven@posteo.de>
2020-02-16 19:25:54 +0100

    Try-out implementation of BrowserRouter.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <br />
+      <Link to="/pagetwo">Go to page two</Link>
+    </div>
+  );
+}
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <br />
+      <Link to="/">Go to page one</Link>
+    </div>
+  );
+}
 
 const App = () => {
-  return <div>Hey from App!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={ PageOne } />
+          <Route path="/pagetwo" component={ PageTwo } />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
