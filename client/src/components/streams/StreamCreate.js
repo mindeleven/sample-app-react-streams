c1cb257c0cb5a39f53a076caec5cd0e19439021f
Jürgen Kober <mindeleven@posteo.de>
2020-02-23 14:44:13 +0100

    Some elaboration on the StreamCreate form.

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    return <input {...formProps.input} />;
+  }
+
   render() {
     return (
-      <div>StreamCreate</div>
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
     )
   }
 }
