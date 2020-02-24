ea6b4e6c5468a1face3e8fab4c1215c45fe72658
Jürgen Kober <mindeleven@posteo.de>
2020-02-24 18:52:54 +0100

    Wiring up the StreamCreate component with the createStream action creator.

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -26,7 +26,7 @@ class StreamCreate extends React.Component {
   }
 
   onSubmit(formValues) {
-    console.log(formValues);
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -65,7 +65,9 @@ const validate = (formValues) => {
   return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate: validate
 })(StreamCreate);
+
+export default connect(null, createStream)(formWrapped);
