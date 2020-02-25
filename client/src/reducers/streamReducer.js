10073a5d85f87809373bc4881db5c16f66680bc5
Jürgen Kober <mindeleven@posteo.de>
2020-02-25 13:24:01 +0100

    Fixed two typos.

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -5,12 +5,12 @@ import {
   FETCH_STREAM,
   EDIT_STREAM,
   DELETE_STREAM
-} from '../action/types';
+} from '../actions/types';
 
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_STREAMS:
-      return { ...state, ..._.mapKeys(action.payload, id) };
+      return { ...state, ..._.mapKeys(action.payload, 'id') };
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
