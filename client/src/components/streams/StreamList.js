05ca9048052a6b0a74be325ed1ddc149ede838e4
Jürgen Kober <mindeleven@posteo.de>
2020-02-26 17:42:21 +0100

    Replacing the functional based component with a class based component and adding a componentDidMount lifecycle mathod.

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+class StreamList extends React.Component {
+  componentDidMount(){
+    this.props.fetchStreams();
+  }
+
+  render() {
+    return <div>StreamList</div>;
+  }
 }
 
 export default connect(null, { fetchStreams })(StreamList);
