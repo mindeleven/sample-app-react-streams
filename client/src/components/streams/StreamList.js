7a27c9391212ac88c63a5382d5464e672050e614
Jürgen Kober <mindeleven@posteo.de>
2020-02-26 17:33:11 +0100

    Wiring up action creator call to StreamList component.

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
 const StreamList = () => {
   return <div>StreamList</div>;
 }
 
-export default StreamList;
+export default connect(null, { fetchStreams })(StreamList);
