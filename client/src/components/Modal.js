dbbc60be1acd081752b9477546935d0ada11a6e8
Jürgen Kober <mindeleven@posteo.de>
2020-03-01 17:43:12 +0100

    Some elaboration on the modal component.

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Modal = props => {
-
+  return ReactDOM.createPortal(
+    <div className="ui dimmer modals visible active">
+      <div className="ui standard modals visible active">
+        bla bla bla bla bla bla bla bla bla bla bla bla
+      </div>
+    </div>,
+    document.querySelector('#modal')
+  );
 }
 
 export default Modal;
