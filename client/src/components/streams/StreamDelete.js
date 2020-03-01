2b6e2c68f1424744b0abd82150b38d6371a8a2bd
Jürgen Kober <mindeleven@posteo.de>
2020-03-01 21:43:02 +0100

    Made the modal component customizable.

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Modal from '../Modal';
 
+const actions = (
+  <React.Fragment>
+    <button className="ui button negative">Delete</button>
+    <button className="ui button">Cancel</button>
+  </React.Fragment>
+);
+
 const StreamDelete = () => {
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+      />
     </div>
   );
 }
